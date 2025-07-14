a01f666703e74800a294175219b6e0e9e3da4cd9
shawnamerson <shawnamerson@gmail.com>
2025-07-14 17:10:34 -0500

    Fixed next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,13 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
-    return [
-      {
-        source: '/socket.io/:path*',
-        destination: 'http://localhost:4000/socket.io/:path*'
-      }
-    ];
-  }
+    // Only apply rewrites in the development environment
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/socket.io/:path*',
+          destination: 'http://localhost:4000/socket.io/:path*',
+        },
+      ];
+    }
+    // Return an empty array for production
+    return [];
+  },
 };
 
 module.exports = nextConfig;
